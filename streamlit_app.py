74f749fb20fa6cdd2edd65c81dc72be1ec7e3113
Mark Peacock <mpeacoc@gmail.com>
2024-01-23 13:37:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ import requests
   
 streamlit.header("Fruityvice Fruit Advice!")
   
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 #streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
