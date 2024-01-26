d0bee1d005b560fc1a1783c0e40fb63294ac2c48
Mark Peacock <mpeacoc@gmail.com>
2024-01-26 16:59:25 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,12 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
+
+
 # --------------- Main Code --------------- #
 streamlit.title("My Mom's New Healthy Diner") 
 
@@ -46,17 +52,18 @@ try:
 
 except URLerror as e: 
   streamlit.error()
+
+
+streamlit.header("The fruit load list contains:")
+
+# Add a button to load the fruit 
+if streamlit.button('Get fruit load list')
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
   
 streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-#my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
 
 # This is dumb , like everything, but think we're just adding a text box to pull an i/p 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
